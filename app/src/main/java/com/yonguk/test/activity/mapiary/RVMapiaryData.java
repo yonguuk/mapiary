d67b73c746c599a5ea6c80159318355e1f95211b
yonguuk <alfhsp@naver.com>
2016-08-11 21:02:12 +0900

    ss

---
+++ app/src/main/java/com/yonguk/test/activity/mapiary/RVMapiaryData.java
@@ -0,0 +1,12 @@
+package com.yonguk.test.activity.mapiary;
+
+/**
+ * Created by dosi on 2016-08-11.
+ */
+public class RVMapiaryData {
+    String date = null;
+    String textTitle = null;
+    String textContent = null;
+
+    String userID = null;
+}
