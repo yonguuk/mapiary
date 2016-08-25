76fe93adb62887bccf02bc578fd0e44d09b0f12d
yonguuk <alfhsp@naver.com>
2016-08-25 17:47:22 +0900

    test video success

--- app/src/main/java/com/yonguk/test/activity/mapiary/subactivity/list/MapiaryListActivity.java
+++ app/src/main/java/com/yonguk/test/activity/mapiary/subactivity/list/MapiaryListActivity.java
@@ -14,14 +14,26 @@ import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 
+import com.android.volley.AuthFailureError;
+import com.android.volley.Request;
 import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonObjectRequest;
 import com.yonguk.test.activity.mapiary.R;
 import com.yonguk.test.activity.mapiary.RVMapiaryAdapter;
 import com.yonguk.test.activity.mapiary.RVMapiaryData;
 import com.yonguk.test.activity.mapiary.network.VolleySingleton;
 import com.yonguk.test.activity.mapiary.subactivity.MapiaryUploadActivity;
 
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
 import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 public class MapiaryListActivity extends AppCompatActivity {
 
@@ -35,6 +47,7 @@ public class MapiaryListActivity extends AppCompatActivity {
     Context mContext = null;
     private final int REQUEST_CODE_UPLOAD_MAPIARY = 1;
     private final String TAG = "MapiaryListActivity";
+    private final String URL_SERVER = "http://kktt0202.dothome.co.kr/master/contents/mapiary_card.php";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -46,7 +59,7 @@ public class MapiaryListActivity extends AppCompatActivity {
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
         getSupportActionBar().setTitle("Mapiary");
-        mapiaryDatas = new ArrayList<RVMapiaryData>();
+        mapiaryDatas = new ArrayList<>();
 
         volleySingleton = VolleySingleton.getInstance(mContext);
         requestQueue = volleySingleton.getRequestQueue();
@@ -60,7 +73,6 @@ public class MapiaryListActivity extends AppCompatActivity {
             }
         };
         mRecyclerView.setLayoutManager(linearLayoutManager);
-        sendJsonRequest();
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -70,6 +82,9 @@ public class MapiaryListActivity extends AppCompatActivity {
                 startActivityForResult(intent, REQUEST_CODE_UPLOAD_MAPIARY);
             }
         });
+
+        sendJsonRequest();
+
     }
 
 
@@ -77,17 +92,69 @@ public class MapiaryListActivity extends AppCompatActivity {
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
         if(requestCode == REQUEST_CODE_UPLOAD_MAPIARY && resultCode == RESULT_OK){
-            RVMapiaryData mapiaryData = new RVMapiaryData();
+/*          RVMapiaryData mapiaryData = new RVMapiaryData();
             mapiaryData.setUserID(data.getStringExtra("USER_ID"));
             mapiaryData.setTextTitle(data.getStringExtra("TITLE"));
             mapiaryData.setTextContent(data.getStringExtra("CONTENT"));
             mapiaryDatas.add(mapiaryData);
-            mRVMapiaryAdapter.setList(mapiaryDatas);
+            mRVMapiaryAdapter.setList(mapiaryDatas);*/
         }
     }
 
     private void sendJsonRequest(){
+        JsonObjectRequest request = new JsonObjectRequest(Request.Method.POST, URL_SERVER, null, new Response.Listener<JSONObject>() {
+            @Override
+            public void onResponse(JSONObject response) {
+                Log.i(TAG,response.toString());
+                mapiaryDatas = parseJsonResponse(response);
+                mRVMapiaryAdapter.setList(mapiaryDatas);
+            }
+        }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                Log.i(TAG,error.getMessage());
+            }
+        }){
+            @Override
+            protected Map<String, String> getParams() throws AuthFailureError {
+                Map<String,String> params = new HashMap<>();
+                return params;
+            }
+        };
+        requestQueue.add(request);
+    }
 
+    private ArrayList<RVMapiaryData> parseJsonResponse(JSONObject response) {
+        ArrayList<RVMapiaryData> list =  new ArrayList<>();
+        if(response != null || response.length() > 0){
+            try{
+                if(response.has("result")){
+                    JSONArray arrayResult = response.getJSONArray("result");
+                    for(int i=0; i<arrayResult.length(); i++){
+                        JSONObject currentResult = arrayResult.getJSONObject(i);
+                        String user_id = currentResult.getString("user_id");
+                        String date = currentResult.getString("date");
+                        String profileImageUrl = currentResult.getString("profile_url");
+                        String textContent = currentResult.getString("content");
+                        String textTitle = currentResult.getString("title");
+                        int like = currentResult.getInt("like");
+
+                        RVMapiaryData mapiary = new RVMapiaryData();
+                        mapiary.setUserID(user_id);
+                        mapiary.setDate(date);
+                        mapiary.setProfileUrl(profileImageUrl);
+                        mapiary.setTextContent(textContent);
+                        mapiary.setTextTitle(textTitle);
+                        list.add(mapiary);
+                    }
+                }
+            }catch (JSONException e) {
+                Log.i(TAG, e.getMessage());
+            } catch (Exception e) {
+                Log.i(TAG, e.getMessage());
+            }
+        }
+        return list;
     }
 
 
